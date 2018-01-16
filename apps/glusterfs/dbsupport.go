ef00adb7cb641e38a107c28ce279cb5fc5b8c056
John Mulligan <jmulligan@redhat.com>
2018-01-16 14:34:02 -0500

    xxx: move db related constants out of app into a more db specific file
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ apps/glusterfs/dbsupport.go
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2018 The heketi Authors
+//
+// This file is licensed to you under your choice of the GNU Lesser
+// General Public License, version 3 or any later version (LGPLv3 or
+// later), or the GNU General Public License, version 2 (GPLv2), in all
+// cases as published by the Free Software Foundation.
+//
+
+
+package glusterfs
+
+const (
+	BOLTDB_BUCKET_CLUSTER          = "CLUSTER"
+	BOLTDB_BUCKET_NODE             = "NODE"
+	BOLTDB_BUCKET_VOLUME           = "VOLUME"
+	BOLTDB_BUCKET_DEVICE           = "DEVICE"
+	BOLTDB_BUCKET_BRICK            = "BRICK"
+	BOLTDB_BUCKET_BLOCKVOLUME      = "BLOCKVOLUME"
+	BOLTDB_BUCKET_DBATTRIBUTE      = "DBATTRIBUTE"
+	DB_CLUSTER_HAS_FILE_BLOCK_FLAG = "DB_CLUSTER_HAS_FILE_BLOCK_FLAG"
+	// default values
+	DB_FILE_NAME = "heketi.db"
+)
