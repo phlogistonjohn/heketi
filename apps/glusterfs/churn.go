f94b7122329abf658d47edc73c0ebe39554c35ae
John Mulligan <jmulligan@redhat.com>
2019-10-31 14:14:29 -0400

    scrape: exhume pending operations too
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/churn.go
+++ apps/glusterfs/churn.go
@@ -19,12 +19,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"os"
-
-	"github.com/boltdb/bolt"
-
-	"github.com/heketi/heketi/pkg/glusterfs/api"
 )
 
 type Extract interface {
@@ -72,6 +67,9 @@ func ChurnOMatic(app *App, trashFile string) error {
 
 	clusterExtractor := newClusterExtractor(&dump)
 	fmt.Printf("Clusters: %v\n", exhume(x, clusterExtractor))
+
+	pendExtractor := newPendingExtractor(&dump)
+	fmt.Printf("Pending Ops: %v\n", exhume(x, pendExtractor))
 
 	fmt.Printf("\n")
 	fp := os.Stdout
@@ -385,139 +383,47 @@ func (ve *clusterExtractor) Report() error {
 	return nil
 }
 
-func churnOnce(app *App, cpass int) error {
-	n := rand.Intn(5)
-	logger.Info("N=%v", n)
-	switch n {
-	case 0, 1, 2, 3:
-		return churnItem(app, cpass, n)
-	case 4:
-		logger.Info("Churn - Doing a run!")
-		c := 10 + rand.Intn(41)
-		x := rand.Intn(4)
-		for i := 0; i < c; i++ {
-			err := churnItem(app, cpass, x)
-			if err != nil {
-				return err
-			}
-		}
-	}
+//---
+type pendingExtractor struct {
+	pending *PendingOperationEntry
+	db      *Db
+}
+
+func newPendingExtractor(db *Db) *pendingExtractor {
+	return &pendingExtractor{db: db}
+}
+
+func (ve *pendingExtractor) Key() []byte {
+	return []byte("PendingOperationEntry")
+}
+
+func (ve *pendingExtractor) BackOffset() int {
+	return 7
+}
+
+func (ve *pendingExtractor) Bounds() (int, int) {
+	return 64, 4096*8
+}
+
+func (ve *pendingExtractor) Reset() error {
+	ve.pending = nil
 	return nil
 }
 
-func churnItem(app *App, cpass, n int) error {
-	switch n {
-	case 0:
-		logger.Info("Churn - Create Volume")
-		return churnCreateVolume(app)
-	case 1:
-		logger.Info("Churn - Delete Volume")
-		return churnDeleteVolume(app)
-	case 2:
-		logger.Info("Churn - Create Block Volume")
-		return churnCreateBlockVolume(app)
-	case 3:
-		logger.Info("Churn - Delete Block Volume")
-		return churnDeleteBlockVolume(app)
-	}
-	return nil
+func (ve *pendingExtractor) Unmarshal(buf []byte) error {
+	ve.pending = NewPendingOperationEntry("x")
+	return ve.pending.Unmarshal(buf)
 }
 
-func churnCreateVolume(app *App) error {
-	req := &api.VolumeCreateRequest{}
-	req.Size = 1 + rand.Intn(10) // TODO wider range
-	req.Durability.Type = api.DurabilityReplicate
-	req.Durability.Replicate.Replica = 3
-
-	vol := NewVolumeEntryFromRequest(req)
-	vc := NewVolumeCreateOperation(vol, app.db)
-	err := RunOperation(vc, app.executor)
-	if err != nil {
-		logger.Err(err)
+func (ve *pendingExtractor) Report() error {
+	if ve.pending == nil {
+		return fmt.Errorf("No pending found")
 	}
-	return nil
-}
-
-func churnDeleteVolume(app *App) error {
-	var vol *VolumeEntry
-	noVols := fmt.Errorf("no vols")
-	hostingVol := fmt.Errorf("hosting vol occupado")
-	err := app.db.View(func(tx *bolt.Tx) error {
-		vl, e := VolumeList(tx)
-		if e != nil {
-			return e
-		}
-		if len(vl) == 0 {
-			return noVols
-		}
-		i := rand.Intn(len(vl))
-		vol, e = NewVolumeEntryFromId(tx, vl[i])
-		if e != nil {
-			return e
-		}
-		if len(vol.Info.BlockInfo.BlockVolumes) > 0 {
-			return hostingVol
-		}
-		return e
-	})
-	if err == noVols || err == hostingVol {
-		logger.Info("no can delete")
-		return nil
+	if ve.pending.Id == "x" {
+		fmt.Printf("%+v", ve.pending)
+		return fmt.Errorf("Incomplete pending")
 	}
-	if err != nil {
-		logger.Err(err)
-		return err
-	}
-
-	vd := NewVolumeDeleteOperation(vol, app.db)
-	err = RunOperation(vd, app.executor)
-	if err != nil {
-		logger.Err(err)
-	}
-	return nil
-}
-
-func churnCreateBlockVolume(app *App) error {
-	req := &api.BlockVolumeCreateRequest{}
-	req.Size = 4 + rand.Intn(17) // TODO wider range
-
-	vol := NewBlockVolumeEntryFromRequest(req)
-	vc := NewBlockVolumeCreateOperation(vol, app.db)
-	err := RunOperation(vc, app.executor)
-	if err != nil {
-		logger.Err(err)
-	}
-	return nil
-}
-
-func churnDeleteBlockVolume(app *App) error {
-	var vol *BlockVolumeEntry
-	noVols := fmt.Errorf("no vols")
-	err := app.db.View(func(tx *bolt.Tx) error {
-		vl, e := BlockVolumeList(tx)
-		if e != nil {
-			return e
-		}
-		if len(vl) == 0 {
-			return noVols
-		}
-		i := rand.Intn(len(vl))
-		vol, e = NewBlockVolumeEntryFromId(tx, vl[i])
-		return e
-	})
-	if err == noVols {
-		logger.Info("No volumes to delete")
-		return nil
-	}
-	if err != nil {
-		logger.Err(err)
-		return err
-	}
-
-	bdel := NewBlockVolumeDeleteOperation(vol, app.db)
-	err = RunOperation(bdel, app.executor)
-	if err != nil {
-		logger.Err(err)
-	}
+	fmt.Printf("D: %s %s\n", ve.pending.Id)
+	ve.db.PendingOperations[ve.pending.Id] = *ve.pending
 	return nil
 }
